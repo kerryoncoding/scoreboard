f53aec8a417b81b5c381ff07176f733444268fe0
kerryoncoding <103139429+kerryoncoding@users.noreply.github.com>
2023-01-29 13:45:16 -0500

    added function to underline leading score (home/guest)

--- src/index.js
+++ src/index.js
@@ -3,35 +3,43 @@ let currentHome = 0;
 let currentGuest = 0;
 let homeScore = document.getElementById("home-score");
 let guestScore = document.getElementById("guest-score");
+let homeLeader = document.getElementById("home");
+let guestLeader = document.getElementById("guest");
 
 function add1home() {
     currentHome += 1;
-    homeScore.innerHTML = currentHome; 
+    homeScore.innerHTML = currentHome;
+    checkleader();  
 }
 
 function add2home() {
     currentHome += 2;
-    homeScore.innerHTML = currentHome; 
+    homeScore.innerHTML = currentHome;
+    checkleader();  
 }
 
 function add3home() {
     currentHome += 3;
-    homeScore.innerHTML = currentHome; 
+    homeScore.innerHTML = currentHome;
+    checkleader();  
 }
 
 function add1guest() {
     currentGuest += 1;
-    guestScore.innerHTML = currentGuest 
+    guestScore.innerHTML = currentGuest;
+    checkleader(); 
 }
 
 function add2guest() {
     currentGuest += 2;
     guestScore.innerHTML = currentGuest; 
+    checkleader();
 }
 
 function add3guest() {
     currentGuest += 3;
     guestScore.innerHTML = currentGuest; 
+    checkleader();
 }
 
 function reset() {
@@ -39,4 +47,20 @@ function reset() {
     currentGuest = 0;
     homeScore.innerHTML = currentHome; 
     guestScore.innerHTML = currentGuest;
+    checkleader();
+}
+
+function checkleader() {
+    if (currentHome > currentGuest) {
+        homeLeader.innerHTML = `<u>HOME</u>`;
+        guestLeader.innerHTML = `GUEST`;
+    } else {
+        if (currentHome < currentGuest) {
+        homeLeader.innerHTML = `HOME`;
+        guestLeader.innerHTML = `<u>GUEST</u>`;
+        } else {
+            homeLeader.innerHTML = `HOME`;
+            guestLeader.innerHTML = `GUEST`;
+        }
+    }
 }
